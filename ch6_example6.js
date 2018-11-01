d8209724fd4f54a04475e924c3b6010eeb0f4317
Publikwerker <jason.michael.hoffman@gmail.com>
2018-11-01 06:35:05 -0700

    more typos
    found some more typos in the book.
    replaced commas with hyphens to allow a range of string lengths.

--- ch6_example6.js
+++ ch6_example6.js
@@ -1,12 +1,12 @@
 // javaScript file for the HTML of the same name
 // very poor design to check for valid phone#, zip, and email
 function isValidTelephoneNumber(telephoneNumber) {
-	var telRegExp = /^(\+\d{1,3} ?)?(\(\d{1,5}\)|\d{1,5})?\d{3}?\d{0,7}(  (x|xtn|ext|extn|pax|pbx|extension)?\.? ?\d{2-5})?$/i;
+	var telRegExp = /^(\+\d{1-3} ?)?(\(\d{1-5}\)|\d{1-5})?\d{3}?\d{0-7}(  (x|xtn|ext|extn|pax|pbx|extension)?\.? ?\d{2-5})?$/i;
 	return telRegExp.test(telephoneNumber);
 }
 
 function isValidPostalCode(postalCode) {
-	var pcodeRegExp = /^(\d{5}(- \d{4})?|([a-z][a-z]?\d\d?|[a-z]{2}\d[a-z])?\d[a-z][a-z])$/i;
+	var pcodeRegExp = /^(\d{5}(-\d{4})?|([a-z][a-z]?\d\d?|[a-z]{2}\d[a-z])?\d[a-z][a-z])$/i;
 	return pcodeRegExp.test(postalCode);
 }
 
